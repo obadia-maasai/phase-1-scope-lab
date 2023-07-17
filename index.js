b1100342d3e5d1f96fbac29f731d3c799998b93c
obadia <obadia.maasai@student.moringaschool.com>
2023-07-18 00:05:10 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = "obadia"
+var bestCustomer
+
+function upperCaseCustomerName 
+ (customerName)
+        'customerName' = customerName.toUpperCase();
+    
+function setBestCustomer(){
+        bestCustomer ='not obadia';
+    }
+function overwriteBestCustomer(){
+    bestCustomer='maybe obadia';
+}    
+
+const leastFevouriteCustomer ="Brian lagat";
+
+function changeLeastFevouriteCustomer(){
+    leastFevouriteCustomer='Ian Alex';
+
+}
+
+
+    
+
+
